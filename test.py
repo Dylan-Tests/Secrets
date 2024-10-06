86120b9606efc7bc603d8e6651118ef985d7c99d
dylan-apiiro <dylan@apiiro.com>
2024-10-06 09:47:53 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,1 @@
-api = "na1.by0yMUdUQTUtbmExL2R5bGFueWFpckBnbWFpbC5jb2069YVPNuegUOJy5f2SEmAEgYJnHvWTX2ybGNdbSMchT3ou/YSZP8MY"
+fullstory_api_secret = "na1.by0yMUdUQTUtbmExL2R5bGFueWFpckBnbWFpbC5jb2069YVPNuegUOJy5f2SEmAEgYJnHvWTX2ybGNdbSMchT3ou/YSZP8MY"
