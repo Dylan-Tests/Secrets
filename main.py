4aef29d664e329b4c1d8481a8c02d881f3e7fcd3
dylan-apiiro <dylan@apiiro.com>
2024-12-15 11:56:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ mapbox_access_token2 = "tk.eyJ1IjoiaGlqaWFuZ3RhbyIsImEiOiJjampxcjFnb3E2NTB5M3BvM
 mapbox_access_token3 = "pk.eyJ1IjoieHdwaXNtZSIsImEiOiJ5cTlCQTlRIn0.QdV-wNUKbgs7jAlbVE747Q"
 MParticle_api = "YWIxMjdi883GHBBDnjsdKAJQxNjdjYUUJABbg6hdI.8V6HhxW-"
 Yotpo_client_secret = "iAF6XvAWnF3mug4zGzLAGA5n6y3JSPPDCYZgTWJ2"
+password = "iAF6XvAWnF3mug4zGzLAGA5n6y3JSPPDCYZgTWJ2"
