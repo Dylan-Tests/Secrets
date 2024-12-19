3a1aa9154f0ac6782ab8d873d46b3f109cb76d7e
dylan-apiiro <dylan@apiiro.com>
2024-12-19 11:51:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 mapbox_access_token = "pk.eyJ1IjoiaGlqaWFuZ3RhbyIsImEiOiJjampxcjFnb3E2NTB5M3253ZHV5YjhjIn0.WneUon5qFigfJRJ3oaZ3Ow"
 password = "pk.eyJ1IjoiaGlqaWFuZ3RhbyIsImEiOiJjampxcjFnb3E2NTB5M3253ZHV5YjhjIn0.WneUon5qFigfJRJ3oaZ3Ow"
+snowflake.jdbc.password = "B95267B8-7667-4505-94B0-945afa0469da"
+password = "B95267B8-7667-4505-94B0-945afa0469da"
