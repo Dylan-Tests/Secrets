b104089277b749533ded6f321ac3ae8342cdbb07
dylan-apiiro <dylan@apiiro.com>
2024-12-22 13:28:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-mapbox_access_token = "pk.eyJ1IjoiaGlqaWFuZ3RhbyIsImEiOiJjampxcjFnb3E2NTB5M3253ZHV5YjhjIn0.WneUon5qFigfJRJ3oaZ3Ow"
-password = "pk.eyJ1IjoiaGlqaWFuZ3RhbyIsImEiOiJjampxcjFnb3E2NTB5M3253ZHV5YjhjIn0.WneUon5qFigfJRJ3oaZ3Ow"
 snowflake.jdbc.password = "B95267B8-7667-4505-94B0-945afa0469da"
 password = "B95267B8-7667-4505-94B0-945afa0469da"
+password2 = "B95267B8-7667-4505-94B0-945afa0469da"
 vercel-token = "sp0Jjdbr8qgwVrlM00FDxHDE"
 password = "sp0Jjdbr8qgwVrlM00FDxHDE"
