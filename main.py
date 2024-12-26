c554c23113c3e2df7e93e02b1078de7a0f723184
dylan-apiiro <dylan@apiiro.com>
2024-12-26 14:13:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 AdobeClientId = "46b8540a316a4hbea54f4d648f835160"
 password = "46b8540a316a4hbea54f4d648f835160"
 password2 = "46b8540a316a4hbea54f4d648f835160"
+AdobeClientId2 = "46b8540a316a4hbea54f4d648f935160"
+password3 = "46b8540a316a4hbea54f4d648f935160"
